99c83064e2954ab0f852ac8687bdbc55e7e55a57
KellyBall <692732163@qq.com>
2021-10-17 15:22:50 +0800

    [update]

--- src/eymain.cpp
+++ src/eymain.cpp
@@ -8,11 +8,29 @@
 using namespace neb;
 using namespace osstd;
 using namespace std;
-
 int main(){
-    system("title EytionLang Shell (20211017a-v0.11)");
+    system("title EytionLang Shell (20211017a-v0.1.3-alpha)");
     //check part
-
+    cout<<"checking setting file..."<<endl;
+    CJsonObject json;
+    ifstream file("./settings/eyconfig.json");
+    istreambuf_iterator<char> begin(file);
+    istreambuf_iterator<char> end;
+    string content(begin, end);
+    json.Parse(content);
+    bool DebugMode, ExitTip;
+    string DependOnConfigFile;
+    if(json.KeyExist("DebugMode") == false || json.KeyExist("DependOnConfigFile") == false || json.KeyExist("ExitTip") == false){
+        cout<<_FONT_RED<<"There is an error in this setting file. Please replace the correct configuration file and restart the program"<<_NORMAL<<endl;
+        Sleep(4000);
+        exit(0);
+    }
+    json.Get("DebugMode", DebugMode);
+    json.Get("DependOnConfigFile", DependOnConfigFile);
+    json.Get("ExitTip", ExitTip);
+    cout<<"done"<<endl;
+    Sleep(1000);
+    system("cls");
     //end
     std::cout<<_FONT_BLUE<<"build date: "<<_FONT_GREEN<<__DATE__<<endl;
     cout<<_NORMAL;
@@ -23,7 +41,8 @@ int main(){
         try{
             cout<<"\n"<<_FONT_YELLOW<<"> "<<_NORMAL;
             getline(cin, cmd);
-            eysys::start_ezcmd(cmd);
+            eysys::start_ezcmd(cmd, eysys::settings{DependOnConfigFile, ExitTip, DebugMode});
+            
         }
         catch(char const* e){cout<<_FONT_YELLOW<<"\nEytionScript has some error:\n    "<<_FONT_RED<<e<<_NORMAL<<endl;}
         catch(std::string e){cout<<_FONT_YELLOW<<"\nEytionScript has some error:\n    "<<_FONT_RED<<e<<_NORMAL<<endl;}
