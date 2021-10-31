b76de7bc7eaea62d2bdef0dfe409dbdfbe16232f
PowerAngelXD <692732163@qq.com>
2021-10-31 14:18:05 +0800

    [update]增加对布尔表达式的优化，事实上，布尔表达式并未完成

--- src/eyexec/executer.cpp
+++ src/eyexec/executer.cpp
@@ -39,7 +39,7 @@ void Executer::run() {
                 break;
             }
             case Instruction::PUSH: {
-                if(ins.op_type == TY_CON) env.push(Environment::runit(Environment::ValueType::STRING, ins.op));
+                if(ins.op_type == TY_CON) env.push(Environment::runit(Environment::ValueType::STRING, ins.op)); //cout<<"\n"<<ins.op<<"-string\n";}
                 else if(ins.op_type == TY_IMM) env.push(Environment::runit(Environment::ValueType::INT, ins.op));
                 else if(ins.op_type == TY_DEC) env.push(Environment::runit(Environment::ValueType::DECI, ins.op));
                 else env.push(Environment::runit(Environment::ValueType::NULLTYPE, ins.op));
@@ -47,21 +47,23 @@ void Executer::run() {
             }
             case Instruction::POP: {
                 if(env.ScopeUnit.findAll(ins.op_str)){
-                    if(env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::Integer){
-                        env.push(Environment::runit(Environment::ValueType::INT, env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsInt()));}
-
-                    else if (env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::Decimal)
-                        env.push(Environment::runit(Environment::ValueType::DECI, env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsDecimal()));
-
-                    else if(env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::String) {
+                    if(env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::String) {
                         env.ConstantPool.push_back(env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsString());
                         env.push(Environment::runit(Environment::ValueType::STRING, env.ConstantPool.size()));
 
                         if(env.instructions.at(pos + 1).ins_type == Instruction::OSOUT){
-                            cout<<env.ConstantPool.at(env.ConstantPool.size() - 1);
+                            cout<<env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsString();
                             pos += 1;
                         }
                     }
+                    else if(env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::Integer){
+                        env.push(Environment::runit(Environment::ValueType::INT, env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsInt()));}
+
+                    else if (env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::Decimal)
+                        env.push(Environment::runit(Environment::ValueType::DECI, env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsDecimal()));
+                    
+                    else if (env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getType() == eytype::EybType::Bool)
+                        env.push(Environment::runit(Environment::ValueType::BOOL, env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).getValueAsBool()));
                 }
                 else{throw EyparseError("NameError", "Unknown Identifier:" + ins.op_str, ins.line, ins.col);}
                 break;
@@ -93,13 +95,13 @@ void Executer::run() {
             case Instruction::EQ: {
                 auto op1 = env.pop().second;
                 auto op2 = env.pop().second;
-                env.push(Environment::runit(Environment::ValueType::DECI, op1 == op2));
+                env.push(Environment::runit(Environment::ValueType::BOOL, op1 == op2));
                 break;
             }
             case Instruction::NEQ: {
                 auto op1 = env.pop().second;
                 auto op2 = env.pop().second;
-                env.push(Environment::runit(Environment::ValueType::DECI, op1 != op2));
+                env.push(Environment::runit(Environment::ValueType::BOOL, op1 != op2));
                 break;
             }
             case Instruction::MOD: {
@@ -117,16 +119,29 @@ void Executer::run() {
                 break;
             }
             case Instruction::OSOUT: {
+                auto temp = env.pop();
                 if (ins.op_type == TY_CON)
-                    std::cout << env.ConstantPool[env.pop().second];
-                else
-                    std::cout << env.pop().second;
+                    std::cout << env.ConstantPool[temp.second];
+                else if (temp.first == Environment::STRING)
+                    std::cout << env.ConstantPool[temp.second];
+                else if (ins.op_type == TY_IMM)
+                    std::cout << temp.second;
+                else if (ins.op_type == TY_DEC)
+                    std::cout << temp.second;
+                else if (temp.first == Environment::DECI)
+                    std::cout << temp.second;
+                else if (temp.first == Environment::INT)
+                    std::cout << temp.second;
+                else if (temp.first == Environment::BOOL)
+                    std::cout << temp.second;
+                else std::cout << env.ConstantPool[temp.second];
                 break;
             }
             case Instruction::DEFINE_VORC: {
                 std::string value_str;
                 int value_int;
                 float value_deci;
+                bool value_bool;
                 bool icst = ins.op_bool; // is const
 
                 int opt;
@@ -138,6 +153,10 @@ void Executer::run() {
                 else if (ins.op_type == TY_IMM){ 
                     value_int = env.pop().second;
                     opt = 1; // int -> 1
+                }
+                else if (ins.op_type == TY_BOL){ 
+                    value_bool = env.pop().second;
+                    opt = 3; // bool -> 3
                 }
                 else {
                     value_deci = env.pop().second;
@@ -153,17 +172,27 @@ void Executer::run() {
                 else if (opt == 2) {
                     env.ScopeUnit.ScopeStack.at(env.ScopeUnit.now).BoardPool.insert(std::pair<std::string, eyv::EyValue>(ins.op_str, eyv::EyValue(value_deci, icst)));
                 }
+                else if (opt == 3) {
+                    env.ScopeUnit.ScopeStack.at(env.ScopeUnit.now).BoardPool.insert(std::pair<std::string, eyv::EyValue>(ins.op_str, eyv::EyValue(value_bool, icst)));
+                }
                 break;
             }
             case Instruction::ASSIGN: {
                 std::string *value = nullptr;
                 int value_int = 9999999;
                 float value_deci = 9999999;
+                bool value_bool = false;
                 if(ins.op_type == TY_CON){
                     value = &env.ConstantPool[env.pop().second];
                 }
                 else if(ins.op_type == TY_IMM){
                     value_int = env.pop().second;
+                }
+                else if(ins.op_type == TY_BOL){
+                    if(env.pop().second == 0){
+                        value_bool = false;
+                    }
+                    else value_bool = true;
                 }
                 else{
                     value_deci = env.pop().second;
@@ -173,6 +202,7 @@ void Executer::run() {
                     if(value != nullptr) env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).setValue(*value);
                     else if(value_int != 9999999) env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).setValue(value_int);
                     else if(value_deci != 9999999) env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).setValue(value_deci);
+                    else env.ScopeUnit.ScopeStack.at(env.ScopeUnit.findWhere(ins.op_str)).BoardPool.at(ins.op_str).setValue(value_bool);
                 }
                 else {throw EyparseError("NameError", "Unknown Identifier:" + ins.op_str, ins.line, ins.col);}
                 break;
