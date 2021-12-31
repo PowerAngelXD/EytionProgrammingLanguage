f6bf7aed374ab27f640834e6b7074891af383cbe
PowerAngelXD <692732163@qq.com>
2021-12-31 23:21:25 +0800

    [new]准备更新模块

---
+++ src/ey/model/eymodel.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../../../eydevkit/edk.h"
+#include "../scope/eyscope.h"
+#include "../../eycodegen/generator.h"
+#include "../../eyexec/executer.h"
+#include "../../eyparser/parser.h"
+#include "../../eyparser/ast.h"
+
+namespace eymdl{
+    
+}
